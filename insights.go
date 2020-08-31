59e9b026b8b1b70e86338fbf07df0fcc5152d9bf
caselongo <caselongo@gmail.com>
2020-08-31 23:41:05 +0200

    x

--- insights.go
+++ insights.go
@@ -27,12 +27,18 @@ type Insight struct {
 
 // Insights return Instagram insights for a user
 //
-func (ig *InstagramGraph) Insights(objectID string, metrics []string, period string, since int64, until int64) (*[]Insight, error) {
+func (ig *InstagramGraph) Insights(objectID string, metrics []string, period *string, since *int64, until *int64) (*[]Insight, error) {
 	params := make(map[string]interface{})
 	params["metric"] = strings.Join(metrics, ",")
-	params["period"] = period
-	params["since"] = since
-	params["until"] = until
+	if period != nil {
+		params["period"] = *period
+	}
+	if since != nil {
+		params["since"] = *since
+	}
+	if until != nil {
+		params["until"] = *until
+	}
 
 	result, err := ig.session.Get(fmt.Sprintf("/%s/insights", objectID), params)
 	if err != nil {
